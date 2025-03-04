993436515683eb99a96333890b23959d1ef647b7
wirlhawk <rayhantogarlumbantobing@gmail.com>
2025-03-04 13:38:54 +0700

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     rules: {
+//       "@typescript-eslint/no-unused-vars": "off",
+//       "next/next/no-img-element": "off",
+//     },
+//   },
+// ];
+
+// export default eslintConfig;
+
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(),
   {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "next/next/no-img-element": "off",
-    },
+    rules: {},
   },
 ];
 
-export default eslintConfig;
